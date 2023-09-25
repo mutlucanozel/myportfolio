a5668b37e70adb9d520bf81c6b900e6370de688c
mutlu-26@hotmail.com <mutlu-26@hotmail.com>
2023-09-25 21:12:52 +0300

    backend conf

--- src/components/Form.js
+++ src/components/Form.js
@@ -44,7 +44,7 @@ const Form = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:3001/submit-form', {
+      const response = await fetch('https://myportfoliobackend-mlj1.onrender.com/submit-form', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
