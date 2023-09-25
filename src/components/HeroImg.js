53af2a079041d8a0918d2644735072cac3ebb9c3
mutlu-26@hotmail.com <mutlu-26@hotmail.com>
2023-09-25 16:38:26 +0300

    changed some UI

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,28 +1,24 @@
-import "./HeroImgStyles.css";
-import React from 'react'
-import kepce from "../assets/kepce.jpeg"
+import React from 'react';
+import kepce from "../assets/kepce.jpeg";
 import { Link } from "react-router-dom";
-
+import "./HeroImgStyles.css"; // Import your CSS file correctly
 
 const HeroImg = () => {
   return (
     <div className="hero">
       <div className="mask">
-        <img className="into-img"
-        src={kepce} alt="IntroImg"/>
+        <img className="into-img" src={kepce} alt="IntroImg" />
+      </div>
+      <div className="content">
+        <p>YAZILIM GELİŞTİRİCİ</p>
+        <h1>Mutlu Can ÖZEL</h1>
+        <div>
+          <Link to="/project" className="btn">Portföy</Link>
+          <Link to="/contact" className="btn btn-light">İLETİŞİM</Link>
         </div>
-        <div className="content">
-            <p> YAZILIM GELİŞTİRİCİ</p>
-            <h1> Mutlu Can ÖZEL</h1>
-            <div>
-           <Link to="/project"
-            className="btn"> Portföy </Link>
-               <Link to="/contact"
-            className="btn btn-light"> İLETİŞİM </Link>
-            </div>
-            </div>    
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeroImg
+export default HeroImg;
