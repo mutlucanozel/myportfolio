e25edd20e7b8293f5bae4e3c453fcca884392aaf
mutlu-26@hotmail.com <mutlu-26@hotmail.com>
2023-09-26 15:27:14 +0300

    s

--- server/server.js
+++ server/server.js
@@ -51,10 +51,22 @@ app.post('/submit-form', async (req, res) => {
 });
 
 // Catch-all route to serve the HTML file for client-side routing
-app.get('*', (req, res) => {
+app.get('/about', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/project', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Catch-all route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
-import React, { useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter, useLocation } from "react-router-dom"; // Import useLocation
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter here
+import App from "./App"; // Import your main App component
 
-function ScrollToTop() {
-  const location = useLocation(); // Get the current location using useLocation
-
-  useEffect(() => {
-    window.scrollTo(0, 0); // Scroll to the top of the page when the location changes
-  }, [location]); // Run the effect whenever the location changes
-
-  return null;
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <BrowserRouter>
-    <ScrollToTop />
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
