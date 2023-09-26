c49d92a235ce4fc338feef99e7504a86f234ab6d
mutlu-26@hotmail.com <mutlu-26@hotmail.com>
2023-09-26 13:08:28 +0300

    deleted  browserrouter

--- src/App.js
+++ src/App.js
@@ -8,16 +8,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-  <>
- <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
-  </>
+
+  <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/about" element={<About/>} />
+    <Route path="/project" element={<Project/>} />
+    <Route path="/contact" element={<Contact/>} />
+    </Routes>
+
   );
 }
 
