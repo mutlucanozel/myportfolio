393316f165acaa6d04bd8bf5aa66c5007578d063
mutlu-26@hotmail.com <mutlu-26@hotmail.com>
2023-09-28 22:44:32 +0300

    added ozgecmıs download

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,10 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { saveAs } from 'file-saver'; 
 import './AboutContentStyles.css';
+import pdfFile from '../assets/mutlucanozelozgecmis.pdf';
 
 
 const AboutContent = () => {
  
+  const handleDownloadClick = () => {
+    // Fetch the PDF file
+    fetch(pdfFile)
+      .then((response) => response.blob())
+      .then((blob) => {
+        // Use file-saver to save the Blob as a file
+        saveAs(blob, 'Mutlu_Can_OZEL_OZGECMIS');
+      })
+      .catch((error) => console.error('Error fetching PDF:', error));
+  };
   return (
     
     <div className="about">
@@ -17,15 +29,19 @@ const AboutContent = () => {
 
 Ben Mutlu Can ÖZEL.Kocaeli Üniversitesi Elektronik ve haberleşme mühendisliği bölümünden Ocak ayında mezun olacağım. iOS geliştirme ve web geliştirme alanına olan ilgim, kariyerimde yeni bir sayfa açmam için beni yazılım alanına yönlendirdi.
  </p> <p>
-Üniversite eğitimim boyunca, temel elektronik ve gömülü yazılım konularının işleyişini derinlemesine inceleme fırsatım oldu. Bu dönemde edindiğim analitik düşünme yeteneği ve problem çözme becerileri, şimdi iOS ve Web geliştirme alanında da beni destekliyor. İlgili bootcampler ve çeşitli kanallardaki içerikler sayesinde HTML, CSS, NodeJS, ReactJS ve Swift gibi teknolojileri öğreniyor ve bu bilgileri pratiğe dökme fırsatı buluyorum.
+Üniversite eğitimim boyunca, temel elektronik ve gömülü yazılım konularının işleyişini derinlemesine inceleme fırsatım oldu. Bu dönemde edindiğim analitik düşünme yeteneği ve problem çözme becerileri, şimdi iOS ve Web geliştirme alanında da beni desteklemekte. İlgili bootcampler ve çeşitli kanallardaki içerikler sayesinde HTML, CSS, NodeJS, ReactJS ve Swift gibi teknolojileri öğreniyor ve bu bilgileri pratiğe dökme fırsatı buluyorum.
 </p>
 <p>
-Mobil ve Web geliştirme konusunda tasarım estetiği ile kullanılabilirlik arasındaki dengeyi kurmaya özen gösteriyorum. Kullanıcıların ihtiyaçlarını anlamak ve bu ihtiyaçları en iyi şekilde karşılayacak arayüzler oluşturmak benim için önemlidir. Ayrıca, takım çalışmasına yatkın bir yapım olduğunu düşünüyorum ve farklı disiplinlerden gelen insanlarla işbirliği yapmayı keyifli buluyorum.</p>
-   <p>Öğrenmeye ve kendimi geliştirmeye olan ilgim, yeni teknolojileri ve trendleri takip ederek devamlı güncel kalma çabamla da kendini gösteriyor.</p>
-        <br></br><br></br><br></br> 
+Mobil ve Web geliştirme konusunda tasarım estetiği ile kullanılabilirlik arasındaki dengeyi kurmaya özen gösteriyor, kullanıcıların ihtiyaçlarını anlamak ve bu ihtiyaçları en iyi şekilde karşılayacak arayüzler oluşturmak için geliştirmeler yapıyorum. Ayrıca, takım çalışmasına yatkın bir yapım olduğunu düşünüyor ve farklı disiplinlerden gelen insanlarla işbirliği yapmayı keyifli buluyorum.</p>
+   <p>Benimle ilgili daha detaylı bilgi sahibi olmak için Özgeçmişime göz gezdirebilirsiniz.</p>
+   <button className="btn4" onClick={handleDownloadClick}>ÖzgeÇMİŞ İndİr </button>
+
+  <br></br><br></br><br></br> 
+  
       </div>
-      
+
     </div>
+    
   );
 };
 
