6be1a5b3d1dd5d7a9b2b05edb34b005ae09fddf8
mutlu-26@hotmail.com <mutlu-26@hotmail.com>
2023-10-09 13:07:07 +0300

    1

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -6,7 +6,7 @@ import PricingCard from '../components/PricingCard'
 import Work from '../components/Work'
 
 const Project = () => {
-  const heroImageSrc = 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2840&q=80';
+  const heroImageSrc = 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2672&q=80';
 
   return (
     <div>
