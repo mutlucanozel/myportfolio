c184d32f3d4e40fb08ab1ad0136cdafb5cf592fa
mutlu-26@hotmail.com <mutlu-26@hotmail.com>
2023-10-09 14:38:21 +0300

    changed background image

--- src/routes/About.js
+++ src/routes/About.js
@@ -6,7 +6,7 @@ import HeroImg2 from '../components/HeroImg2';
 import AboutContent from '../components/AboutContent';
 
 const About = () => {
-    const heroImageSrc = 'https://images.unsplash.com/photo-1573495613179-848ffd29f962?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2938&q=80';
+    const heroImageSrc = 'https://images.unsplash.com/photo-1520607162513-77705c0f0d4a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2938&q=80';
 
     return (
         <div>
