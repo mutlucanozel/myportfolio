92e55f215c435457b22daf8d5969094eb5888dd0
Mutlu Can BYB <mutlu@bybetiket.com>
2024-12-01 17:18:02 +0300

    Commit mesajınız

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -27,19 +27,25 @@ const AboutContent = () => {
         <h1>Ben Kimim?</h1>
         <p>Merhaba,
 
-Ben Mutlu Can ÖZEL. Kocaeli Üniversitesi Elektronik ve haberleşme mühendisliği bölümünden Ocak ayında mezun olacağım. iOS geliştirme ve web geliştirme alanına olan ilgim, kariyerimde yeni bir sayfa açmam için beni yazılım alanına yönlendirdi.
+Ben Mutlu Can ÖZEL. Kocaeli Üniversitesi Elektronik ve Haberleşme mühendisliği bölümünden mezun oldum ve 2024 haziran ayından beri BYB etiket bünyesinde yazılım sorumlusu olarak çalışmaktayım.
  </p> <p>
-Üniversite eğitimim boyunca, temel elektronik ve gömülü yazılım konularının işleyişini derinlemesine inceleme fırsatım oldu. Bu dönemde edindiğim analitik düşünme yeteneği ve problem çözme becerileri, şimdi iOS ve Web geliştirme alanında da beni desteklemekte. İlgili bootcampler ve çeşitli kanallardaki içerikler sayesinde HTML, CSS, NodeJS, ReactJS ve Swift gibi teknolojileri öğreniyor ve bu bilgileri pratiğe dökme fırsatı buluyorum.
+Üniversite eğitimim boyunca, temel elektronik ve gömülü yazılım konularının işleyişini derinlemesine inceleme fırsatım oldu. Bu dönemde edindiğim analitik düşünme yeteneği ve problem çözme becerileri, şimdi Web geliştirme alanında da beni desteklemekte. İlgili bootcampler ve çeşitli kanallardaki içerikler sayesinde HTML, CSS, ASP.CORE MVC gibi teknolojileri öğreniyor ve bu bilgileri pratiğe dökme fırsatı buluyorum.
 </p>
 <p>
-Mobil ve Web geliştirme konusunda tasarım estetiği ile kullanılabilirlik arasındaki dengeyi kurmaya özen gösteriyor, kullanıcıların ihtiyaçlarını anlamak ve bu ihtiyaçları en iyi şekilde karşılayacak arayüzler oluşturmak için geliştirmeler yapıyorum. Ayrıca, takım çalışmasına yatkın bir yapım olduğunu düşünüyor ve farklı disiplinlerden gelen insanlarla işbirliği yapmayı keyifli buluyorum.</p>
-   <p>Benimle ilgili daha detaylı bilgi sahibi olmak için özgeçmişime göz gezdirebilirsiniz.</p>
-   <button className="btn4" onClick={handleDownloadClick}>ÖzgeÇMİŞ İndİr </button>
+Mobil ve Web geliştirme konusunda tasarım estetiği ile kullanılabilirlik arasındaki dengeyi kurmaya özen gösteriyor, kullanıcıların ihtiyaçlarını anlamak ve bu ihtiyaçları en iyi şekilde karşılayacak arayüzler oluşturmak için  responsive geliştirmeler yapıyorum. Ayrıca, takım çalışmasına yatkın bir yapım olduğunu düşünüyor ve farklı disiplinlerden gelen insanlarla işbirliği yapmayı keyifli buluyorum.</p>
+   <p>Benimle ilgili daha detaylı bilgi sahibi olmak için linkedin profilime göz gezdirebilirsiniz.</p>
+   <button 
+    className="btn4" 
+    onClick={() => window.open('https://www.linkedin.com/in/mutlucanozel/', '_blank')}
+>
+    LİNKEDİN ADRESİ
+</button>
+
 
   <br></br><br></br><br></br> 
   
       </div>
-
+ 
     </div>
     
   );
